c209078934d720d7e94dbaf75ff193787ebc8405
Pierre Abbat <phma@bezitopo.org>
2026-06-17 06:45:15 +0000

    Draw the areaBelow computation

--- src/HalftoneFunction.jl
+++ src/HalftoneFunction.jl
@@ -3,7 +3,7 @@ using QuadGK,Roots
 export tryFunc1,areaBelow
 
 # The halftone function h(x) is defined as follows:
-# h(x) increases from 0 to 1.
+# h(x) increases as x goes from 0 to 1.
 # The area of {(x,y) in [0,1]×[0,1]: h(x)h(y)<z} is z whenever z is in [0,1].
 # h(-x)=-h(x).
 # h(1-x)=h(x).
@@ -12,6 +12,18 @@ export tryFunc1,areaBelow
 function tryFunc1(x::AbstractFloat)
   (1-(x-1)^2)^(2/3)
 end
+
+# +-----------------+
+# |      *          |
+# |      *          |
+# |       *         |
+# |diagCross*       |
+# |         | *     |
+# |         |   ****|
+# |         |       |
+# |         |       |
+# +-----------------+
+
 
 function areaBelow(h::Function,z::AbstractFloat)
   diagCross=find_zero(x->h(x)-sqrt(z),[0,1])
