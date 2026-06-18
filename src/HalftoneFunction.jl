61f6368728a16e17f29f29125d44b887384ca1f6
Pierre Abbat <phma@bezitopo.org>
2026-06-18 08:45:54 +0000

    Pull out a copy of integrand to find the bug

--- src/HalftoneFunction.jl
+++ src/HalftoneFunction.jl
@@ -1,6 +1,7 @@
 module HalftoneFunction
 using QuadGK,Roots
 export tryFunc1,areaBelow
+export outegrand
 
 # The halftone function h(x) is defined as follows:
 # h(x) increases as x goes from 0 to 1.
@@ -23,6 +24,11 @@ end
 # |         |       |
 # |         |       |
 # +-----------------+
+
+function outegrand(h::Function,z::T,y::T) where T<:AbstractFloat
+  diagCross=find_zero(x->h(x)-sqrt(z),[0,1])
+  diagCross,find_zero(x->h(x)-z/y,[diagCross,1])
+end
 
 function areaBelow(h::Function,z::AbstractFloat)
   diagCross=find_zero(x->h(x)-sqrt(z),[0,1])
